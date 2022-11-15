a513480b34ef32130d6955666d7035e913bc98d0
DimiDime <ilev997@gmail.com>
2022-11-15 19:39:24 +0100

    Last

--- microblog-app/microblog-app/microblog/main.py
+++ microblog-app/microblog-app/microblog/main.py
@@ -20,21 +20,4 @@ def index():
             2, user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
         ),
     ]
-    return render_template("main/index.html", posts=posts)
-
-
-
-
-user = model.User(email="mary@example.com", name="mary")
-posts = [
-    model.Message(
-        user=user,
-        text="Test post",
-        timestamp=datetime.datetime.now(dateutil.tz.tzlocal()),
-    ),
-    model.Message(
-        user=user,
-        text="Another post",
-        timestamp=datetime.datetime.now(dateutil.tz.tzlocal()),
-    ),
-]
+    return render_template("homePage.html", posts=posts)
