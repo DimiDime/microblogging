fb333ca2c95af26191df4a925cb81b4b6a018844
DimiDime <ilev997@gmail.com>
2022-11-15 23:22:00 +0100

    last

--- microblog-app/microblog-app/microblog/__init__.py
+++ microblog-app/microblog-app/microblog/__init__.py
@@ -14,28 +14,6 @@ def create_app(test_config=None):
 # so that they are easier to maintain. 
 
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-def create_app(test_config=None):
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = b"\x8c\xa5\x04\xb3\x8f\xa1<\xef\x9bY\xca/*\xff\x12\xfb"
-    app.config[
-        "SQLALCHEMY_DATABASE_URI"
-    ] = "mysql+mysqldb://23_webapp_XX:YYYYYYYYY@mysql.lab.it.uc3m.es/23_webapp_XXa"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-
-    # Register blueprints:
-    from . import main
-
-    app.register_blueprint(main.bp)
-    return app
-
-
 #    /usr/lab/alum/0491346/Descargas/microblog-app
 #cd Descargas/microblog-app
 #flask --debug run
--- microblog-app/microblog-app/microblog/main.py
+++ microblog-app/microblog-app/microblog/main.py
@@ -10,7 +10,7 @@ bp = Blueprint("main", __name__)
 
 
 @bp.route("/")
-def index():
+def index_home():
     user = model.User(1, "mary@example.com", "mary")
     posts = [
         model.Message(
@@ -21,3 +21,35 @@ def index():
         ),
     ]
     return render_template("homePage.html", posts=posts)
+
+@bp.route("/user")
+def index_user():
+    user2 = model.User(2, "hans@example.com", "hans")
+    posts = [
+        model.Message(
+            1, user2, "Test post", datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            2, user2, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            3, user2, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+    ]
+    return render_template("userProfile.html", posts=posts)
+
+@bp.route("/message")
+def index_message():
+    user2 = model.User(2, "hans@example.com", "hans")
+    posts = [
+        model.Message(
+            1, user2, "Answer", datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            2, user2, "Answer", datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            3, user2, "Answer", datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+    ]
+    return render_template("userProfile.html", posts=posts)
