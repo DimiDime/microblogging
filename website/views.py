46d1f7abf9cc8b80596809558b588a219e69ede4
Dimi <ilev997@gmail.com>
2022-11-23 12:03:35 +0100

    page

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 # Main views or URL front end aspect of webpage
-from flask import Blueprint
+from flask import Blueprint, render_template
 # This file is a blueprint of our application
 # Has a lot of roots defined (buncha URLs)
 
@@ -8,5 +8,5 @@ views = Blueprint('views', __name__) # Blueprint for flask application
 @views.route('/')
 # This function will run when we go to the / root
 def home():
-  return "<h1>Test</h1>"
+  return render_template("home.html")
 
